e7b173efd2f8a6fd0888edc08479391102b5e43b
ryan <truongducthang@gmail.com>
2015-07-13 22:47:25 +0800

    Explicitly make the Logger inside logable a slf4j logger

--- src/main/scala/hnct/lib/utility/Logable.scala
+++ src/main/scala/hnct/lib/utility/Logable.scala
@@ -1,11 +1,12 @@
 package hnct.lib.utility
 
 import org.slf4j.LoggerFactory
+import org.slf4j.Logger
 
 trait Logable {
 	
 	self =>
 		
-	final val log = LoggerFactory.getLogger(self.getClass.getName)
+	final val log : Logger = LoggerFactory.getLogger(self.getClass.getName)
 	
 }
